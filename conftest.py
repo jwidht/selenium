d87b6a76c6d773b58cb34565ddc2192cffdae75c
Learn By Heart Public Repository <iguyenum-ru@yahoo.com>
2025-04-29 20:19:49 +0700

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='ru', #None, #'en',
+                     help="Choose language en or ru")
+                    
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart browser for test..")
+    user_language = request.config.getoption("language")
+    print(f"user_language={user_language}")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+   
+    
